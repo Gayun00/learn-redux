aa3eccdb6ce331b47214c31324c215e7217e44b8
gy <gayunlee11@gmail.com>
2021-11-25 14:21:07 +0900

    feat: store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// **** (1) createStore 와 루트 리듀서 불러오기
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
 import registerServiceWorker from './registerServiceWorker';
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
